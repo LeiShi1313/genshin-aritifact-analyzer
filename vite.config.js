d0f3ccbef4daddb570254e77f9528c4e65945910
Lei Shi <me@leishi.io>
2023-01-16 10:48:45 +0100

    add sentry source map

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sentryVitePlugin from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: "caosen",
+      project: "genshin-artifact-builds",
+
+      // Specify the directory containing build artifacts
+      include: "./dist",
+
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and needs the `project:releases` and `org:read` scopes
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+
+      // Optionally uncomment the line below to override automatic release name detection
+      // release: process.env.RELEASE,
+    }),
+  ],
+});
