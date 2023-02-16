0588d0ef2f41116c764081858f7971f43ff8653e
Lei Shi <me@leishi.io>
2023-02-16 10:41:50 +0100

    fix crimsonWitch

--- src/utils/set.ts
+++ src/utils/set.ts
@@ -5,6 +5,7 @@ export const monaSetToSet = (setName: string): Set => {
   if (setName === 'exile') return Set.THE_EXILE;
   if (setName === 'wandererTroupe') return Set.WANDERERS_TROUPE;
   if (setName === 'shimenawaReminiscence') return Set.SHIMENAWAS_REMINISCENCE;
+  if (setName === 'crimsonWitch') return Set.CRIMSON_WITCH_OF_FLAMES;
 
   const name = setName.charAt(0).toLowerCase() + setName.slice(1);
   const key = name.replace(/[A-Z]/g, x => `_${x}`).toUpperCase();
