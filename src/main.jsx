a292d75353fa5b877955ed017eb9c3d9c7a26118
Lei Shi <me@leishi.io>
2025-11-14 11:44:07 +0100

    remove sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -11,38 +9,12 @@ import "./i18n";
 import { store, persistor } from "./store";
 
 if (!import.meta.env.DEV) {
-  Sentry.init({
-    dsn: "https://fb6ab464e545427480a8baac61205322@o176406.ingest.sentry.io/4504485933613056",
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.1,
-
-    replaysSessionSampleRate: 0.1,
-
-    replaysOnErrorSampleRate: 1.0,
-
-    integrations: [new BrowserTracing(), new Sentry.Replay()],
-
-    beforeSend(event, hint) {
-      console.log(hint.originalException);
-      const error = hint.originalException;
-      if (
-        error &&
-        error.message &&
-        (error.message.match(
-          /Failed to fetch dynamically imported module/i
-        ) ||
-          error.message.match(
-            /error loading dynamically imported module/i
-          ))
-      ) {
-        return null;
-      }
-      return event;
-    },
-  });
+  // Load tracking script
+  const trackingScript = document.createElement("script");
+  trackingScript.src = "https://track.leishi.io/api/script.js";
+  trackingScript.setAttribute("data-site-id", "2");
+  trackingScript.defer = true;
+  document.head.appendChild(trackingScript);
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import sentryVitePlugin from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -12,24 +11,5 @@ export default ({ mode }) => {
     ],
   };
 
-  if (process.env.NODE_ENV !== "development") {
-    config["build"] = {
-      sourcemap: true
-    }
-    config.plugins.push(sentryVitePlugin({
-      org: "caosen",
-      project: "genshin-artifact-builds",
-
-      // Specify the directory containing build artifacts
-      include: "./dist",
-
-      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-      // and needs the `project:releases` and `org:read` scopes
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-
-      // Optionally uncomment the line below to override automatic release name detection
-      // release: process.env.RELEASE,
-    }));
-  }
   return defineConfig(config);
 }
